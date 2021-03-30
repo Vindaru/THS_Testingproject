5bfd7dd78d17cfafb9679f324be9a0b4b93277a3
vindaru <vinay4daru@gmail.com>
2021-03-30 10:44:21 -0400

    updated project

--- src/main/java/com/THSpages/OccupancyPage.java
+++ src/main/java/com/THSpages/OccupancyPage.java
@@ -20,154 +20,154 @@ import junit.framework.Assert;
  */
 public class OccupancyPage extends BasePage {
 
-    private static Logger log = Logger.getLogger(OccupancyPage.class.getName() + " ----------------------------------");
-
-    /*
-     * Occupancy page will be testing, occupancies based on states
-     * 
-     * - verify if occupancy label is displaying - based on states verify if all the
-     * occupancies are displaying or not
-     */
-
-    private By Occupancy = By.name("policyTerm|location|occupancy");
-
-    public WebElement getOccupancy() {
-	return getElement(Occupancy);
-    }
-
-    public void OccupancyValues(String state, String occupancy) throws InterruptedException {
+	private static Logger log = Logger.getLogger(OccupancyPage.class.getName() + " ----------------------------------");
 
 	/*
-	 * VERIFY IF ALL THE OCCUPANCIES ARE DISPALYING BASED ON STATES
+	 * Occupancy page will be testing, occupancies based on states
 	 * 
-	 * =============================================================================
-	 * 
-	 * AL : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * AZ : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * AR : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * GA : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * IL : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * IN : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * LA : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * MS : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * MI : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * MO : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * TN : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * OH : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * TX : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * NC : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * SC : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * WI : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
-	 * -----------------------------------------------------------------------------
-	 * ----------------------------------------------------------------------------
-	 * 
-	 * =============================================================================
-	 * =============================================================================
-	 * =======
-	 * 
+	 * - verify if occupancy label is displaying - based on states verify if all the
+	 * occupancies are displaying or not
 	 */
-	switch (state) {
 
-	case "AL":    // Alabama 
-	case "AR":    // Arkansas
-	case "GA":    // Georgia
-	case "IN":    // Indiana
-	case "IL":    // Illinios
-	case "LA":    // Louisana
-	case "MI":    // Michigan
-	case "MO":    // Missouri
-	case "MS":    // Mississippi
-	case "OH":    // Ohio
-	case "TN":    // Tennessee
-	case "WI":    // Wiscosin
-	    
-	    String List_1[] = { "Select...","Seasonal", "Vacant", "Rental", "Owner", "Not A Residence" };
-	    Select set1_selectOccupancy = new Select(driver.findElement(Occupancy));
-	    List<WebElement> set1_dropdowns = set1_selectOccupancy.getOptions();
-	    log.info(set1_dropdowns.size());
-	    for (int i = 0; i < set1_dropdowns.size(); i++) {
-		Assert.assertEquals(set1_dropdowns.get(i).getText(), List_1[i]);
-		log.info(set1_dropdowns.get(i).getText());
-	    }
-	    log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
-	    Thread.sleep(500);
-	    set1_selectOccupancy.selectByVisibleText(occupancy);
-	    break; 
-	case "AZ":   // Arizona
-	case "SC":   // South Carolina
-	case "TX":   // Texas
-	    
-	    String List_2[] = { "Select...", "Seasonal", "Vacant", "Rental", "Owner", "Not A Residence", "Tenant" };
-	    Select set2_selectOccupancy = new Select(driver.findElement(Occupancy));
-	    List<WebElement> set2_dropdowns = set2_selectOccupancy.getOptions();
+	private By Occupancy = By.name("policyTerm|location|occupancy");
 
-	    log.info(set2_dropdowns.size());
-
-	    for (int i = 0; i < set2_dropdowns.size(); i++) {
-		Assert.assertEquals(set2_dropdowns.get(i).getText(), List_2[i]);
-		log.info(set2_dropdowns.get(i).getText());
-	    }
-	    log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
-	    Thread.sleep(500);
-	    set2_selectOccupancy.selectByVisibleText(occupancy);
-	    break;
-
-	case "NC":   // North Carolina
-
-	    String List_3[] = { "Select...", "Seasonal", "Rental", "Owner" };
-	    Select set3_selectOccupancy = new Select(driver.findElement(Occupancy));
-	    List<WebElement> set3_dropdowns = set3_selectOccupancy.getOptions();
-
-	    log.info(set3_dropdowns.size());
-
-	    for (int i = 0; i < set3_dropdowns.size(); i++) {
-		Assert.assertEquals(set3_dropdowns.get(i).getText(), List_3[i]);
-		log.info(set3_dropdowns.get(i).getText());
-	    }
-	    log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
-	    Thread.sleep(500);
-	    set3_selectOccupancy.selectByVisibleText(occupancy);
-	    break;
+	public WebElement getOccupancy() {
+		return getElement(Occupancy);
 	}
-    }
 
-    public void SelectOccupancy(String state, String occupancy) throws InterruptedException {
+	public void OccupancyValues(String state, String occupancy) throws InterruptedException {
 
-	Select selectOccupancy = new Select(driver.findElement(Occupancy));
-	selectOccupancy.selectByVisibleText(occupancy);
-    }
+		/*
+		 * VERIFY IF ALL THE OCCUPANCIES ARE DISPALYING BASED ON STATES
+		 * 
+		 * =============================================================================
+		 * 
+		 * AL : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * AZ : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * AR : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * GA : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * IL : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * IN : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * LA : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * MS : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * MI : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * MO : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * TN : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * OH : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * TX : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * NC : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * SC : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE || TENANT
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * WI : OWNER || SEASONAL || RENTAL || VACANT || NOT A RESIDENCE
+		 * -----------------------------------------------------------------------------
+		 * ----------------------------------------------------------------------------
+		 * 
+		 * =============================================================================
+		 * =============================================================================
+		 * =======
+		 * 
+		 */
+		switch (state) {
 
-    public OccupancyPage(WebDriver driver) {
-	super(driver);
-    }
+		case "AL": // Alabama
+		case "AR": // Arkansas
+		case "GA": // Georgia
+		case "IN": // Indiana
+		case "IL": // Illinios
+		case "LA": // Louisana
+		case "MI": // Michigan
+		case "MO": // Missouri
+		case "MS": // Mississippi
+		case "OH": // Ohio
+		case "TN": // Tennessee
+		case "WI": // Wiscosin
+
+			String List_1[] = { "Select...", "Seasonal", "Vacant", "Rental", "Owner", "Not A Residence" };
+			Select set1_selectOccupancy = new Select(driver.findElement(Occupancy));
+			List<WebElement> set1_dropdowns = set1_selectOccupancy.getOptions();
+			log.info(set1_dropdowns.size());
+			for (int i = 0; i < set1_dropdowns.size(); i++) {
+				Assert.assertEquals(set1_dropdowns.get(i).getText(), List_1[i]);
+				log.info(set1_dropdowns.get(i).getText());
+			}
+			log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
+			Thread.sleep(500);
+			set1_selectOccupancy.selectByVisibleText(occupancy);
+			break;
+		case "AZ": // Arizona
+		case "SC": // South Carolina
+		case "TX": // Texas
+
+			String List_2[] = { "Select...", "Seasonal", "Vacant", "Rental", "Owner", "Not A Residence", "Tenant" };
+			Select set2_selectOccupancy = new Select(driver.findElement(Occupancy));
+			List<WebElement> set2_dropdowns = set2_selectOccupancy.getOptions();
+
+			log.info(set2_dropdowns.size());
+
+			for (int i = 0; i < set2_dropdowns.size(); i++) {
+				Assert.assertEquals(set2_dropdowns.get(i).getText(), List_2[i]);
+				log.info(set2_dropdowns.get(i).getText());
+			}
+			log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
+			Thread.sleep(500);
+			set2_selectOccupancy.selectByVisibleText(occupancy);
+			break;
+
+		case "NC": // North Carolina
+
+			String List_3[] = { "Select...", "Seasonal", "Rental", "Owner" };
+			Select set3_selectOccupancy = new Select(driver.findElement(Occupancy));
+			List<WebElement> set3_dropdowns = set3_selectOccupancy.getOptions();
+
+			log.info(set3_dropdowns.size());
+
+			for (int i = 0; i < set3_dropdowns.size(); i++) {
+				Assert.assertEquals(set3_dropdowns.get(i).getText(), List_3[i]);
+				log.info(set3_dropdowns.get(i).getText());
+			}
+			log.info("Verifiaction for Occupancy applicability is not compelte for state : " + state);
+			Thread.sleep(500);
+			set3_selectOccupancy.selectByVisibleText(occupancy);
+			break;
+		}
+	}
+
+	public void SelectOccupancy(String state, String occupancy) throws InterruptedException {
+
+		Select selectOccupancy = new Select(driver.findElement(Occupancy));
+		selectOccupancy.selectByVisibleText(occupancy);
+	}
+
+	public OccupancyPage(WebDriver driver) {
+		super(driver);
+	}
 }
--- src/test/java/com/THStest/BasicInformationPageTest.java
+++ src/test/java/com/THStest/BasicInformationPageTest.java
@@ -29,9 +29,10 @@ import junit.framework.Assert;
  */
 public class BasicInformationPageTest extends BaseTest {
 
-    private static Logger log = Logger
-	    .getLogger(BasicInformationPageTest.class.getName() + " ----------------------------------");
-    // header
+	private static Logger log = Logger
+			.getLogger(BasicInformationPageTest.class.getName() + " ----------------------------------");
+
+	// header
 //    @Test(priority = 1,  description = "Testcase-1(BasicInformation page) = 'After landing on BasicInformationPage verify header ='RATING & POLICY MANAGEMENT' ")
 //    public void verify_BasicinformationPage_header() throws InterruptedException {
 //	page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
@@ -45,62 +46,63 @@ public class BasicInformationPageTest extends BaseTest {
 //        Assert.assertEquals(homepageheader, "Address Validator");
 //	Thread.sleep(1000);
 //    }
-    // verify lables displaying
-    @Test(priority = 1, description = "Testcase-6(BasicInformationPage) = 'After landing on BasicInformationPage verify all labels are displayin as expected.")
-    public void verify_BasicinformationPage_Labels() throws InterruptedException {
+	// verify lables displaying
+	@Test(priority = 1, description = "Testcase-6(BasicInformationPage) = 'After landing on BasicInformationPage verify all labels are displayin as expected.")
+	public void verify_BasicinformationPage_Labels() throws InterruptedException {
 
-	page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
-	Thread.sleep(300);
-	page.getInstance(PortalPage.class).PLP_CLICK();
-	Thread.sleep(300);
-	page.getInstance(DisconnectPage.class).DisconnectSession_button();
-	Thread.sleep(300);
-	// homepage select state
-	page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
+		page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
+		Thread.sleep(300);
+		page.getInstance(PortalPage.class).PLP_CLICK();
+		Thread.sleep(300);
+		page.getInstance(DisconnectPage.class).DisconnectSession_button();
+		Thread.sleep(300);
+		// homepage select state
+		page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
 
-	page.getInstance(BasicInformationPage.class).getBasicinformationpageLables();
+		page.getInstance(BasicInformationPage.class).getBasicinformationpageLables();
 
-    }
+	}
 
-    @Test(priority = 2)
-    public void verify_BasicinformationPage_ExelData() throws InterruptedException {
-	page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
-	Thread.sleep(300);
-	page.getInstance(PortalPage.class).PLP_CLICK();
-	Thread.sleep(300);
-	page.getInstance(DisconnectPage.class).DisconnectSession_button();
-	Thread.sleep(300);
-	// homepage select state
-	page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
-	// basic information page
-	Thread.sleep(500);
-	page.getInstance(BasicInformationPage.class).doBasicinformationPagevalidation(FirstName, LastName, RiskAddress,
-		RiskCity, RiskZipcode, DOB, Occupancy, FireHyderate, RiskState, YearBuilt, ReneovatedHome, PolicyForm);
-	Thread.sleep(1000);
-    }
+	@Test(priority = 2)
+	public void verify_BasicinformationPage_ExelData() throws InterruptedException {
+		page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
+		Thread.sleep(300);
+		page.getInstance(PortalPage.class).PLP_CLICK();
+		Thread.sleep(300);
+		page.getInstance(DisconnectPage.class).DisconnectSession_button();
+		Thread.sleep(300);
+		// homepage select state
+		page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
+		// basic information page
+		Thread.sleep(500);
+		page.getInstance(BasicInformationPage.class).doBasicinformationPagevalidation(FirstName, LastName, RiskAddress,
+				RiskCity, RiskZipcode, DOB, Occupancy, FireHyderate, RiskState, YearBuilt, ReneovatedHome, PolicyForm);
+		Thread.sleep(1000);
+	}
+
 //    dependsOnGroups = {"Use test Data : 'TowerHillTestData' "}
-    @Test( priority = 3)
-    public void verify_BasicinformationPage_DataProvider() throws InterruptedException {
-	page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
-	Thread.sleep(300);
-	page.getInstance(PortalPage.class).PLP_CLICK();
-	Thread.sleep(300);
-	page.getInstance(DisconnectPage.class).DisconnectSession_button();
-	Thread.sleep(300);
-	// homepage select state
-	page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
-	// basic information page
-	Thread.sleep(500);
-	page.getInstance(BasicInformationPage.class).doBasicinformationPagevalidation(FirstName, LastName, RiskAddress,
-		RiskCity, RiskZipcode, DOB, Occupancy, FireHyderate, RiskState, YearBuilt, ReneovatedHome, PolicyForm);
-	Thread.sleep(1000);
-    }
-    
-    @DataProvider
-    public Object[][] getLoginData() throws EncryptedDocumentException, InvalidFormatException, IOException {
-	Object data[][] = TestUtil.getTowerHillTestData("login");
+	@Test(priority = 3)
+	public void verify_BasicinformationPage_DataProvider() throws InterruptedException {
+		page.getInstance(LoginPage.class).doLogin(GroupID, UseName, Password);
+		Thread.sleep(300);
+		page.getInstance(PortalPage.class).PLP_CLICK();
+		Thread.sleep(300);
+		page.getInstance(DisconnectPage.class).DisconnectSession_button();
+		Thread.sleep(300);
+		// homepage select state
+		page.getInstance(HomePage.class).doSelectStateByDataprovided(RiskState);
+		// basic information page
+		Thread.sleep(500);
+		page.getInstance(BasicInformationPage.class).doBasicinformationPagevalidation(FirstName, LastName, RiskAddress,
+				RiskCity, RiskZipcode, DOB, Occupancy, FireHyderate, RiskState, YearBuilt, ReneovatedHome, PolicyForm);
+		Thread.sleep(1000);
+	}
 
-	return data;
-    }
-  
+	@DataProvider
+	public Object[][] getLoginData() throws EncryptedDocumentException, InvalidFormatException, IOException {
+		Object data[][] = TestUtil.getTowerHillTestData("login");
+
+		return data;
+	}
+
 }
